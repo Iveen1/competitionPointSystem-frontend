b9a83476a6b11b958eafbc2dc1cfb960d90d0520
Anton Polyakov <antonpolyakovvv@mail.ru>
2022-11-12 19:13:19 +0300

    feature: in progress of points

--- src/app/participants/participant-page/participant-page.component.ts
+++ src/app/participants/participant-page/participant-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ParticipantsService} from "../../services/participants.service";
 import {ActivatedRoute, Router} from "@angular/router";
-import {CreateParticipant} from "../../info/CreateParticipant";
+import {PointsService} from "../../services/points.service";
+import {TasksService} from "../../services/tasks.service";
 
 @Component({
   selector: 'app-participant-page',
@@ -11,9 +12,11 @@ import {CreateParticipant} from "../../info/CreateParticipant";
 export class ParticipantPageComponent implements OnInit {
   id: number = 0;
   participant: any;
-  // @ts-ignore
-  private createParticipant: CreateParticipant;
-  constructor(private participantsService: ParticipantsService, private router: Router, private route: ActivatedRoute) { }
+  tasks: any;
+  points: any;
+  table: Map<number, any> = new Map();
+  isLoaded = false;
+  constructor(private participantsService: ParticipantsService, private pointsService: PointsService, private tasksService: TasksService, private router: Router, private route: ActivatedRoute) { }
 
   redirectUnknownPage() {
     this.router.navigate(['404']);
@@ -34,8 +37,37 @@ export class ParticipantPageComponent implements OnInit {
       this.id = id;
       // @ts-ignore
       this.participant = item['body'];
+      this.getTasks();
+      this.getPoints(id);
     }, error => {
       this.redirectUnknownPage();
     });
+  }
+
+  getTasks() {
+    this.tasksService.getTasks(0, 1000).subscribe((item: any) => {
+      this.tasks = item['body']['content'];
+    })
+  }
+
+  getPoints(id: number) {
+    this.pointsService.getPointsByParticipantId(id).subscribe((item: any) => {
+      // @ts-ignore
+      this.points = Object.assign({}, ...item['body']['points'].map((x: any) => ({[x.task.id]: x})));
+      this.generateTable();
+      this.isLoaded = true;
+    })
+  }
+
+  generateTable() {
+    for (let task of this.tasks) {
+      let taskPoint = this.points[task['id']];
+      if (taskPoint == undefined) {
+        taskPoint = 0;
+      }
+
+      this.table.set(task.id, taskPoint)
+    }
+    console.log(this.table)
   }
 }
