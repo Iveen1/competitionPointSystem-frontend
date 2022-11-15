d00b80f5f2096b3f6778c09ac698bccd2fe9e57e
Iveen <antonpolyakovvv@mail.ru>
2022-11-16 00:20:41 +0300

    feature: added diagrams on main page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ParticipantsService} from "../services/participants.service";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private participantsService: ParticipantsService) { }
+  page = 0;
+  pagesInfo: number = -1;
+  data: any = [];
 
   ngOnInit(): void {
+    this.getParticipants()
   }
 
+  getParticipants() {
+    this.data = [];
+    this.participantsService.getParticipants(this.page, 5).subscribe((items: any) => {
+      console.log(items)
+      this.pagesInfo = items.body.totalPages;
+      for (let item of items.body.content) {
+        // @ts-ignore
+        this.data.push({name: `${item["firstName"]} ${item["lastName"]}`, value: this.countPoints(item)});
+      }
+      console.log(this.data)
+      this.data = [...this.data];
+    })
+  }
+
+  countPoints(data: any) {
+    let points = 0;
+    for (let point of data['points']) {
+      points += point['coefficient'] * point['task']['maxPoints'];
+    }
+    return points;
+  }
+
+  nextPage() {
+    if (this.page < this.pagesInfo) {
+      this.page += 1;
+      this.getParticipants();
+    }
+  }
+
+  prevPage() {
+    if (this.page >= 1){
+      this.page -= 1;
+      this.getParticipants();
+    }
+  }
 }
